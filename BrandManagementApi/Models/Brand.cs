28a70e1394300eadfc5aebe755af920395028feb
stackoverthinking <tronghieunguyen7242@gmail.com>
2026-03-08 21:08:39 +0700

    adding CreatedAt property to Brand model

--- BrandManagementApi/Models/Brand.cs
+++ BrandManagementApi/Models/Brand.cs
@@ -5,12 +5,15 @@ namespace BrandCRUDProject.Models
     public class Brand
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(100)]
         public required string Name { get; set; }
+
         [Required]
         [MaxLength(100)]
         public required string Category { get; set; }
         public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
