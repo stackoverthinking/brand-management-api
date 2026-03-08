f270cd02c068d6fa0f993faaf6593955fc08a13f
stackoverthinking <tronghieunguyen7242@gmail.com>
2026-03-08 21:26:20 +0700

    add controller

---
+++ BrandManagementApi/Controllers/BrandsController.cs
@@ -0,0 +1,97 @@
+﻿using BrandCRUDProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrandCRUDProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandContext _dbContext;
+
+        public BrandController(BrandContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            var brands = await _dbContext.Brands.ToListAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            var brand = await _dbContext.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(brand);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _dbContext.Brands.Add(brand);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBrand), new { id = brand.Id }, brand);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if (id != brand.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BrandExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            var brand = await _dbContext.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Brands.Remove(brand);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BrandExists(int id)
+        {
+            return _dbContext.Brands.Any(e => e.Id == id);
+        }
+    }
+}
