59a075bd9cd8b5adfdcc28172b78610c72a3ea27
stackoverthinking <tronghieunguyen7242@gmail.com>
2026-03-08 21:21:22 +0700

    edit connection string name in program.cs

--- BrandManagementApi/Program.cs
+++ BrandManagementApi/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<BrandContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BrandCS")));
+builder.Services.AddDbContext<BrandContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
