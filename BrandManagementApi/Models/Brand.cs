381f5a21859ffbac8561c01c884760b3eb2d2e3c
stackoverthinking <tronghieunguyen7242@gmail.com>
2026-03-08 20:59:53 +0700

    Initital commit

---
+++ BrandManagementApi/Models/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrandCRUDProject.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public int IsActive { get; set; }
+    }
+}
