1a48b82190bb42b5ca690aab648d11825ed3faa6
stackoverthinking <tronghieunguyen7242@gmail.com>
2026-03-08 21:04:29 +0700

    adding Validation Attribute to Name and Category in BrandModel

--- BrandManagementApi/Models/Brand.cs
+++ BrandManagementApi/Models/Brand.cs
@@ -5,7 +5,11 @@ namespace BrandCRUDProject.Models
     public class Brand
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public required string Name { get; set; }
+        [Required]
+        [MaxLength(100)]
         public required string Category { get; set; }
         public int IsActive { get; set; }
     }
